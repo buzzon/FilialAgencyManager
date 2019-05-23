93d211a63238cec7facf909d380d6389cd3b99eb
_buzz0n <buzzondev@gmail.com>
2019-05-23 23:13:21 +0500

    TableManager refactoring

--- Client_WindowsForms/TableManager.cs
+++ Client_WindowsForms/TableManager.cs
@@ -7,73 +7,71 @@ using System.Windows.Forms;
 
 namespace Client_WindowsForms
 {
-    class TableManager
+    internal class TableManager
     {
         private delegate void TableFiller();
-        private readonly Dictionary<DataGridView, TableFiller> tablesFillers = new Dictionary<DataGridView, TableFiller>();
+        private readonly Dictionary<DataGridView, TableFiller> _tablesFillers = new Dictionary<DataGridView, TableFiller>();
 
         public TableManager(DataGridView[] tables)
         {
-            TableFiller[] TableFillers = GetTableFillers(tables);
-            for (int i = 0; i < tables.Length; i++)
-                tablesFillers[tables[i]] = TableFillers[i];
+            var tableFillers = GetTableFillers(tables);
+            for (var i = 0; i < tables.Length; i++)
+                _tablesFillers[tables[i]] = tableFillers[i];
         }
 
 
-        public static void InitializeTables(DataGridView[] Tables)
+        public static void InitializeTables(DataGridView[] tables)
         {
-            Tables[0].Rows.Add("Промышленность", "0", "0", "0");
-            Tables[0].Rows.Add("Население", "0", "0", "0");
-            Tables[0].Rows.Add("Бюджет", "0", "0", "0");
-            Tables[0].Rows.Add("ОПП, ЖКХ и др.", "0", "0", "0");
-            Tables[0].Rows.Add("Прочее", "0", "0", "0");
-            Tables[1].Rows.Add("0", "0", "0", "0");
-            Tables[2].Rows.Add("Физическое", "0", "0", "0");
-            Tables[2].Rows.Add("Юридическое", "0", "0", "0");
-            Tables[3].Rows.Add("Физическое", "0", "0", "1", "0");
-            Tables[3].Rows.Add("Юридическое", "0", "0", "1", "0");
-            Tables[4].Rows.Add("0", "0", "0", "0", "0");
-            Tables[5].Rows.Add("0");
-            Tables[6].Rows.Add("0", "0", "0");
-            Tables[7].Rows.Add("Инциденты", "0", "0", "0");
-            Tables[7].Rows.Add("Техника безопасности", "0", "0", "0");
-            Tables[8].Rows.Add("0", "0", "0");
-            Tables[9].Rows.Add("0", "0", "0");
+            tables[0].Rows.Add("Промышленность", "0", "0", "0");
+            tables[0].Rows.Add("Население", "0", "0", "0");
+            tables[0].Rows.Add("Бюджет", "0", "0", "0");
+            tables[0].Rows.Add("ОПП, ЖКХ и др.", "0", "0", "0");
+            tables[0].Rows.Add("Прочее", "0", "0", "0");
+            tables[1].Rows.Add("0", "0", "0", "0");
+            tables[2].Rows.Add("Физическое", "0", "0", "0");
+            tables[2].Rows.Add("Юридическое", "0", "0", "0");
+            tables[3].Rows.Add("Физическое", "0", "0", "1", "0");
+            tables[3].Rows.Add("Юридическое", "0", "0", "1", "0");
+            tables[4].Rows.Add("0", "0", "0", "0", "0");
+            tables[5].Rows.Add("0");
+            tables[6].Rows.Add("0", "0", "0");
+            tables[7].Rows.Add("Инциденты", "0", "0", "0");
+            tables[7].Rows.Add("Техника безопасности", "0", "0", "0");
+            tables[8].Rows.Add("0", "0", "0");
+            tables[9].Rows.Add("0", "0", "0");
         }
 
         public void FillTable(DataGridView table)
         {
-            tablesFillers[table]();
+            _tablesFillers[table]();
         }
 
-        private bool TryParseCells<T>(DataGridViewCell[] cells, out T[] tableParams) where T : IComparable
+        private static bool TryParseCells<T>(IReadOnlyList<DataGridViewCell> cells, out T[] tableParams) where T : IComparable
         {
-            Type t = typeof(T);
-            var method = t.GetMethod("TryParse", new Type[] { typeof(string), typeof(T).MakeByRefType() });
+            var t = typeof(T);
+            var method = t.GetMethod("TryParse", new [] { typeof(string), typeof(T).MakeByRefType() });
 
-            tableParams = new T[cells.Length];
-            for (int i = 0; i < cells.Length; i++)
+            tableParams = new T[cells.Count];
+            for (var i = 0; i < cells.Count; i++)
             {
-                var parametrs = new object[] { cells[i].Value.ToString(), null };
-                if (!(bool)method.Invoke(null, parametrs) || tableParams[i].CompareTo(default(T)) < 0)
+                var parameters = new object[] { cells[i].Value.ToString(), null };
+                if (method != null && (!(bool)method.Invoke(null, parameters) || tableParams[i].CompareTo(default(T)) < 0))
                     return false;
-                tableParams[i] = (T)parametrs[1];
+                tableParams[i] = (T)parameters[1];
             }
             return true;
         }
 
         private TableFiller[] GetTableFillers(DataGridView[] tables)
         {
-            TableFiller[] tablesFillers = new TableFiller[tables.Length];
+            var tablesFillers = new TableFiller[tables.Length];
             tablesFillers[0] = () =>
             {
-                DataGridViewCell[] cells;
-                for (int i = 0; i < tables[0].RowCount; i++)
+                for (var i = 0; i < tables[0].RowCount; i++)
                 {
-                    cells = new DataGridViewCell[] { tables[0][1, i], tables[0][2, i] };
-                    if (!TryParseCells(cells, out double[] parameters) || parameters[1] <= 0)
-                        continue;
-                    tables[0][3, i].Value = parameters[0]  / parameters[1] * 366;
+                    var cells = new[] {tables[0][1, i], tables[0][2, i]};
+                    if (!TryParseCells(cells, out double[] parameters) || parameters[1] <= 0) continue;
+                    tables[0][3, i].Value = parameters[0] / parameters[1] * 366;
                 }
             };
 
@@ -100,14 +98,12 @@ namespace Client_WindowsForms
 
             tablesFillers[2] = () =>
             {
-                int documentsCount = 0;
-                DataGridViewCell[] cells;
+                var documentsCount = 0;
                 for (var i = 0; i < tables[2].RowCount; i++)
                 {
-                    cells = new DataGridViewCell[] { tables[2][1, i], tables[2][2, i] };
-                    if (!TryParseCells(cells, out int[] parameters) || parameters[1] <= 0)
-                        continue;
-                    tables[2][3, i].Value = (double)parameters[0] / parameters[1];
+                    var cells = new [] { tables[2][1, i], tables[2][2, i] };
+                    if (!TryParseCells(cells, out int[] parameters) || parameters[1] <= 0) continue;
+                    tables[2][3, i].Value = (double) parameters[0] / parameters[1];
                     documentsCount += parameters[1];
                 }
                 tables[6][1, 0].Value = documentsCount;
@@ -116,13 +112,11 @@ namespace Client_WindowsForms
 
             tablesFillers[3] = () =>
             {
-                DataGridViewCell[] cells;
                 for (var i = 0; i < tables[3].RowCount; i++)
                 {
-                    cells = new DataGridViewCell[] { tables[3][1, i], tables[3][2, i], tables[3][3, i] };
-                    if (!TryParseCells(cells, out int[] parameters) || parameters[1] <= 0)
-                        continue;
-                    tables[3][4, i].Value = (double)parameters[0] / parameters[1] * parameters[2] * 100;
+                    var cells = new [] { tables[3][1, i], tables[3][2, i], tables[3][3, i] };
+                    if (!TryParseCells(cells, out int[] parameters) || parameters[1] <= 0) continue;
+                    tables[3][4, i].Value = (double) parameters[0] / parameters[1] * parameters[2] * 100;
                 }
                 tables[4][0, 0].Value = tables[3][2, 0].Value;
                 tables[4][1, 0].Value = tables[3][2, 1].Value;
@@ -132,10 +126,8 @@ namespace Client_WindowsForms
             tablesFillers[4] = () =>
             {
                 DataGridViewCell[] cells = { tables[4][0, 0], tables[4][1, 0], tables[4][3, 0] };
-                if (!TryParseCells(cells, out int[] parameters))
-                    return;
-                if (parameters[2] > 0)
-                    tables[4][4, 0].Value = (double)(parameters[0] + parameters[1]) / parameters[2];
+                if (!TryParseCells(cells, out int[] parameters) || parameters[2] <= 0) return;
+                tables[4][4, 0].Value = (double) (parameters[0] + parameters[1]) / parameters[2];
             };
 
             tablesFillers[5] = () => { };
@@ -143,20 +135,17 @@ namespace Client_WindowsForms
             tablesFillers[6] = () =>
             {
                 DataGridViewCell[] cells = { tables[6][0, 0], tables[6][1, 0] };
-                if (!TryParseCells(cells, out int[] parameters) || parameters[1] <= 0)
-                    return;
-                tables[6][2, 0].Value = (double)parameters[0]  / parameters[1] * 1000000;
+                if (!TryParseCells(cells, out int[] parameters) || parameters[1] <= 0) return;
+                tables[6][2, 0].Value = (double) parameters[0] / parameters[1] * 1000000;
             };
 
             tablesFillers[7] = () =>
             {
-                DataGridViewCell[] cells;
-                for (int i = 0; i < tables[7].Rows.Count; i++)
+                for (var i = 0; i < tables[7].Rows.Count; i++)
                 {
-                    cells = new DataGridViewCell[] { tables[7][1, i], tables[7][2, i] };
-                    if (!TryParseCells(cells, out int[] parameters) || parameters[1] <= 0)
-                        continue;
-                    tables[7][3, i].Value = (double)parameters[0] / parameters[1] * 100;
+                    var cells = new [] { tables[7][1, i], tables[7][2, i] };
+                    if (!TryParseCells(cells, out int[] parameters) || parameters[1] <= 0) continue;
+                    tables[7][3, i].Value = (double) parameters[0] / parameters[1] * 100;
                 }
             };
 
