f89ac7de72ffe8773201fe0d7903d7ce7e738d1b
Mo <emptyset2017@gmail.com>
2019-05-31 14:39:11 +0500

    Форматирование таблиц от Павла

--- libs/ExcelManager.cs
+++ libs/ExcelManager.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Excel = Microsoft.Office.Interop.Excel;
+using System.Drawing;
 
 namespace Libs
 {
@@ -43,21 +44,40 @@ namespace Libs
             var sheet = (Excel.Worksheet)ExcelApp.Worksheets.Item[1];
             sheet.Name = "Отчет за " + quarter.Split('-')[0] + " " + subsidiary;
 
-            ExcelApp.Columns.ColumnWidth = 30;
+            //ExcelApp.Columns.ColumnWidth = 30;
 
-            ExcelApp.Cells[1, 1] = quarter + " " + subsidiary; ;
+            var startRow = 1;
+            for (var i = 0; i<Tables.Length; i++)
+            {
+                TableHeader(Tables, ref startRow, i);
+                FillTable(Tables[i], ref startRow);
+            }
 
-            var startRow = 3;
-            foreach (var table in Tables)
-                FillTable(table, ref startRow);
+        }
+
+        private static void TableHeader(DataGridView[] Tables, ref int startRow, int i)
+        {
+            Excel.Range range = ExcelApp.Range[ExcelApp.Cells[startRow, 1], ExcelApp.Cells[startRow, Tables[i].ColumnCount]];
+            range.Merge(Type.Missing);
+            range.Cells.Font.Size = 12;
+            range.Font.Bold = true;
+            range.EntireRow.AutoFit();
+            range.HorizontalAlignment = Excel.XlHAlign.xlHAlignCenter;
+            ExcelApp.Cells[startRow, 1] = nameTables[i];
+            startRow++;
+
         }
 
         private static void FillTable(DataGridView table, ref int startRow)
         {
+            DrawingTable(table, startRow);
             //storing headers
             for (var i = 0; i < table.ColumnCount; i++)
             {
-                ExcelApp.Cells[startRow, i + 1] = table.Columns[i].HeaderText;
+                ExcelApp.Cells[startRow, i + 1]  = table.Columns[i].HeaderText;
+                ExcelApp.Cells[startRow, i + 1].EntireRow.Font.Bold = true;
+                ExcelApp.Cells[startRow, i + 1].Interior.ColorIndex = 15;
+                ExcelApp.Columns[i + 1].AutoFit();               
             }
 
             startRow++;
@@ -67,6 +87,33 @@ namespace Libs
                 for (var j = 0; j < table.RowCount; j++)
                     ExcelApp.Cells[startRow + j, i + 1] = table[i, j].Value.ToString();
             startRow += table.RowCount + 1;
+
+        }
+
+        private static void DrawingTable(DataGridView table, int startRow)
+        {
+            Excel.Range range = ExcelApp.Range[ExcelApp.Cells[startRow, 1], 
+                ExcelApp.Cells[startRow + table.RowCount, table.ColumnCount]];
+            range.Borders.get_Item(Excel.XlBordersIndex.xlEdgeBottom).LineStyle =
+                Excel.XlLineStyle.xlContinuous;
+            range.Borders.get_Item(Excel.XlBordersIndex.xlEdgeBottom).Weight =
+                Excel.XlBorderWeight.xlThick;
+            range.Borders.get_Item(Excel.XlBordersIndex.xlEdgeTop).LineStyle =
+                Excel.XlLineStyle.xlContinuous;
+            range.Borders.get_Item(Excel.XlBordersIndex.xlEdgeTop).Weight = 
+                Excel.XlBorderWeight.xlThick;
+            range.Borders.get_Item(Excel.XlBordersIndex.xlEdgeRight).LineStyle =
+                Excel.XlLineStyle.xlContinuous;
+            range.Borders.get_Item(Excel.XlBordersIndex.xlEdgeRight).Weight =
+                Excel.XlBorderWeight.xlThick;
+            range.Borders.get_Item(Excel.XlBordersIndex.xlEdgeLeft).LineStyle = 
+                Excel.XlLineStyle.xlContinuous;
+            range.Borders.get_Item(Excel.XlBordersIndex.xlEdgeLeft).Weight = 
+                Excel.XlBorderWeight.xlThick;
+            range.Borders.get_Item(Excel.XlBordersIndex.xlInsideHorizontal).LineStyle = 
+                Excel.XlLineStyle.xlContinuous;
+            range.Borders.get_Item(Excel.XlBordersIndex.xlInsideVertical).LineStyle = 
+                Excel.XlLineStyle.xlContinuous;
         }
 
         private static void Save()
@@ -76,5 +123,20 @@ namespace Libs
             ExcelApp.Quit();
             Dialog?.Dispose();
         }
+
+        private static string[] nameTables = new string[]
+        {
+            "ОБОРАЧИВАЕМОСТЬ ДЗ ПО ОТРАСЛЯМ",
+            "ВЫПОЛНЕНИЕ ПЛАНА ПО ПОГАШЕНИЮ ПРОСРОЧЕННОЙ ДЗ",
+            "ВЫРУЧКА НВД НА ОДНОГО КЛИЕНТА",
+            "КАЧЕСТВО БИЛЛИНГА",
+            "ПРОИЗВОДИТЕЛЬНОСТЬ",
+            "КАЧЕСТВО ПРЕДОСТАВЛЯЕМЫХ УСЛУГ",
+            "КАЧЕСТВО СОБЛЮДЕНИЯ НОРМАТИВНО-ПРАВОВОЙ БАЗЫ",
+            "БЕЗОПАСНОСТЬ ТРУДА",
+            "КАЧЕСТВО РАБОТЫ С ПЕРСОНАЛОМ",
+            "КАЧЕСТВО РАБОТЫ С ПЕРСОНАЛОМ"
+        };
+
     }
 }
